befb6745f9f26486d378389ea63d039042a64ee6
Achal Bhute <aachalbhute@gmail.com>
2017-10-08 23:06:05 +0530

    commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {ProcessService} from '../process.service'
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,20 @@ import {ProcessService} from '../process.service'
 export class HeaderComponent implements OnInit {
 
   @Input() hero:any;
-
+   $: any;
   constructor(private _process : ProcessService) {
-    
+  
    }
   ngOnInit() {   
   }
 
+  ngAfterViewInit(){
+   $('#title').editable({
+      type:  'text',
+      pk:    1,
+      name:  'title',
+      url:   'post.php',  
+      title: 'Enter title'
+   });
+  }
 }
